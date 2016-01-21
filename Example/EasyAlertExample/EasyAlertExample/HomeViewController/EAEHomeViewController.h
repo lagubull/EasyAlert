6f4d253d0f6fb955a14d1a112f458c807b648f5f
Javier Laguna <lagubull@hotmail.com>
2016-01-21 17:33:03 +0000

    added example first commit

---
+++ Example/EasyAlertExample/EasyAlertExample/HomeViewController/EAEHomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  EAEHomeViewController.h
+//  EasyAlertExample
+//
+//  Created by Javier Laguna on 21/01/2016.
+//  Copyright © 2016 Javier Laguna. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EAEHomeViewController : UIViewController
+
+@end
---
+++ Example/EasyAlertExample/EasyAlertExample/HomeViewController/EAEHomeViewController.m
@@ -0,0 +1,22 @@
+//
+//  EAEHomeViewController.m
+//  EasyAlertExample
+//
+//  Created by Javier Laguna on 21/01/2016.
+//  Copyright © 2016 Javier Laguna. All rights reserved.
+//
+
+#import "EAEHomeViewController.h"
+
+@interface EAEHomeViewController ()
+
+@end
+
+@implementation EAEHomeViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+@end
